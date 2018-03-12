b66a864f4010e698e81cdffa73ff1d79e1318ac9
WeizeSUn <1155062041@link.cuhk.edu.hk>
2018-03-12 21:43:42 +0800

    Not bug free, not tested

--- src/main/java/fyp/StreamingK.java
+++ src/main/java/fyp/StreamingK.java
@@ -1,11 +1,15 @@
 package fyp;
 
 import org.apache.flink.metrics.*;
+import org.apache.flink.streaming.api.datastream.*;
+import org.apache.flink.streaming.api.environment.*;
+import org.apache.flink.streaming.api.*;
 import org.apache.flink.api.common.functions.*;
 import org.apache.flink.api.common.state.*;
 import org.apache.flink.api.common.typeinfo.*;
 import org.apache.flink.api.java.tuple.*;
 import org.apache.flink.configuration.*;
+import org.apache.flink.util.*;
 
 import java.util.*;
 import java.util.Map.*;
@@ -13,37 +17,40 @@ import java.util.Map.*;
 import java.io.*;
 
 public class StreamingK {
-  private class ClassifyFunction extends
+  private int kTarget = 30;
+
+  private static class ClassifyFunction extends
     RichMapFunction<Object, Tuple2<Element, Long>> {
 
     private MapState<Long, Element> centroids;
     private ValueState<Long> count;
     private ValueState<Double> f;
 
-    private int k;
     private Random rand = new Random();
 
     public void open(Configuration config) {
       MapStateDescriptor<Long, Element> centroidsDescriptor
         = new MapStateDescriptor<>(
-          "centroids",
-          TypeInformation.of(new TypeHint<Long>() {}),
-          TypeInformation.of(new TypeHint<Element>() {})
-        );
+        "centroids",
+        TypeInformation.of(new TypeHint<Long>() {}),
+        TypeInformation.of(new TypeHint<Element>() {})
+      );
       this.centroids = getRuntimeContext().getMapState(centroidsDescriptor);
 
       ValueStateDescriptor<Long> countDescriptor
-          = new ValueStateDescriptor<>(
-                  "count",
-                  TypeInformation.of(new TypeHint<Long>() {}),
-                  0L);
+        = new ValueStateDescriptor<>(
+        "count",
+        TypeInformation.of(new TypeHint<Long>() {}),
+        0L
+      );
       this.count = getRuntimeContext().getState(countDescriptor);
 
       ValueStateDescriptor<Double> fDescriptor
-          = new ValueStateDescriptor<>(
-                  "f",
-                  TypeInformation.of(new TypeHint<Double>() {}),
-                  -1.0);
+        = new ValueStateDescriptor<>(
+        "f",
+        TypeInformation.of(new TypeHint<Double>() {}),
+        -1.0
+      );
       this.f = getRuntimeContext().getState(fDescriptor);
     }
 
@@ -80,21 +87,17 @@ public class StreamingK {
         return new Tuple2(element, cluster);
       }
     }
+  }
 
-    public ClassifyFunction(int k) {
-      this.k = k;
+  private static class ClassifiedFilter
+    implements FilterFunction<Tuple2<Element, Long>> {
+    @Override
+    public boolean filter(Tuple2<Element, Long> tuple) throws Exception {
+      return tuple.f1 > 0;
     }
   }
 
-  private class ClassifiedFilter
-    implements FilterFunction<Tuple2<Element, Long>> {
-      @Override
-      public boolean filter(Tuple2<Element, Long> tuple) throws Exception {
-        return tuple.f1 > 0;
-      }
-  }
-
-  private class UnclassifiedFilter
+  private static class UnclassifiedFilter
     implements FilterFunction<Tuple2<Element, Long>> {
       @Override
       public boolean filter(Tuple2<Element, Long> tuple) throws Exception {
@@ -102,5 +105,216 @@ public class StreamingK {
       }
   }
 
-  public StreamingK(int k) {}
+  private static class CentroidsGenerator
+    extends RichMapFunction<Tuple2<Element, Long>,
+      Tuple3<Long, Element, Double>> {
+
+      private ValueState<Double> f;
+      private ValueState<Long> count;
+      private ValueState<Long> q;
+      private MapState<Long, Element> centroids;
+
+      private final Random rand = new Random();
+      private final int k;
+      private final int kTarget;
+      private boolean initialized = false;
+
+      public Tuple3<Long, Element, Double> map(Tuple2<Element, Long> tuple)
+        throws Exception {
+        long cnt = count.value();
+        Element element = tuple.f0;
+        double ff = f.value();
+        if (cnt < k + 10) {
+          count.update(cnt + 1);
+          centroids.put(cnt + 1, element);
+          return new Tuple3(cnt + 1, element, ff);
+        }
+        if (!initialized && cnt == k + 10) {
+          initialized = true;
+          PriorityQueue<Double> heap
+            = new PriorityQueue<Double>(10, Collections.reverseOrder());
+          LinkedList list = new LinkedList<Element>();
+          for (Element elem: centroids.values()) {
+            list.add(elem);
+          }
+          Collection<Double> allValues
+            = Element.allDistancesInSet(list);
+          for (double temp: allValues) {
+            heap.add(temp);
+            if (heap.size() > 10) {
+              heap.poll();
+            }
+          }
+          double sum = 0;
+          for (double temp: heap) {
+            sum += temp * temp;
+          }
+          f.update(sum / 2);
+        }
+        long cluster = -1;
+        double minDistance = Double.POSITIVE_INFINITY;
+        for (Map.Entry<Long, Element> entry: centroids.entries()) {
+          double dist = element.distance(entry.getValue());
+          if (dist < minDistance) {
+            cluster = entry.getKey();
+            minDistance = dist;
+          }
+        }
+        if (Math.min(minDistance * minDistance / ff, 1)
+          >= rand.nextDouble()) {
+            centroids.put(cnt + 1, element);
+            long qq = q.value();
+            if (qq + 1 >= k) {
+              q.update(0L);
+              f.update(ff * 10);
+            } else {
+              q.update(qq + 1);
+            }
+            return new Tuple3(cnt + 1, element, f.value());
+        } else {
+          return new Tuple3(cluster, element, -ff);
+        }
+      }
+
+      public void open(Configuration config) {
+        MapStateDescriptor<Long, Element> centroidsDescriptor
+          = new MapStateDescriptor<>(
+          "centroids",
+          TypeInformation.of(new TypeHint<Long>() {}),
+          TypeInformation.of(new TypeHint<Element>() {})
+        );
+        this.centroids = getRuntimeContext().getMapState(centroidsDescriptor);
+
+        ValueStateDescriptor<Long> countDescriptor
+          = new ValueStateDescriptor<>(
+          "count",
+          TypeInformation.of(new TypeHint<Long>() {}),
+          0L
+        );
+        this.count = getRuntimeContext().getState(countDescriptor);
+
+        ValueStateDescriptor<Double> fDescriptor
+          = new ValueStateDescriptor<>(
+          "f",
+          TypeInformation.of(new TypeHint<Double>() {}),
+          0.0
+        );
+        this.f = getRuntimeContext().getState(fDescriptor);
+
+        ValueStateDescriptor<Long> qDescriptor
+          = new ValueStateDescriptor<>(
+          "q",
+          TypeInformation.of(new TypeHint<Long>() {}),
+          0L
+        );
+        this.q = getRuntimeContext().getState(qDescriptor);
+      }
+
+      public CentroidsGenerator(int kTarget) {
+        this.kTarget = kTarget;
+        this.k = (kTarget - 11) / 5;
+      }
+  }
+
+  public static class NewCentroids
+    implements FilterFunction<Tuple3<Long, Element, Double>> {
+    @Override
+    public boolean filter(Tuple3<Long, Element, Double> tuple)
+      throws Exception {
+        return tuple.f2 > 0;
+    }
+  }
+
+  public static class RealPoints
+    implements FilterFunction<Tuple3<Long, Element, Double>> {
+    @Override
+    public boolean filter(Tuple3<Long, Element, Double> tuple)
+      throws Exception {
+        return tuple.f2 < 0;
+    }
+  }
+
+  public static class RealPointsToResult
+    implements MapFunction<Tuple3<Long, Element, Double>,
+      Tuple2<Element, Long>> {
+    @Override
+    public Tuple2<Element, Long> map(Tuple3<Long, Element, Double> tuple)
+      throws Exception {
+      return new Tuple2(tuple.f1, tuple.f0);
+    }
+  }
+
+  public static class CentroidsDistributor
+    implements FlatMapFunction<Tuple3<Long, Element, Double>, Object> {
+    private final int parallelism;
+
+    @Override
+    public void flatMap(Tuple3<Long, Element, Double> tuple,
+      Collector<Object> out)
+        throws Exception {
+      for (int i = 0; i < parallelism; i++) {
+        out.collect(new Tuple4(i, tuple.f0, tuple.f1, tuple.f2));
+      }
+    }
+
+    public CentroidsDistributor(int parallelism) {
+      this.parallelism = parallelism;
+    }
+  }
+
+  public StreamingK(int kTarget) {
+    if (kTarget < 16) {
+        System.out.println("Warning: the k value is too small, " +
+                "will be automatically set to 16");
+        kTarget = 16;
+    }
+    this.kTarget = kTarget;
+  }
+
+  public static void main(String[] args) throws Exception {
+    String inputPath = args[0], outputPath = args[1];
+    int parallelism = 1, kTarget = 30;
+    final StreamExecutionEnvironment env
+      = StreamExecutionEnvironment.getExecutionEnvironment();
+
+    DataStream<String> source = env.readTextFile(inputPath);
+
+    IterativeStream<Object> parsed
+      = source.map(new MapFunction<String, Object>() {
+      @Override
+      public Object map(String value) throws Exception {
+        String[] stringArray = value.split(",");
+        double[] doubleArray = new double[stringArray.length];
+        for (int i = 0; i < stringArray.length; i++) {
+          doubleArray[i] = Double.parseDouble(stringArray[i]);
+        }
+        return new Element(doubleArray);
+      }
+    }).iterate();
+
+    DataStream<Tuple2<Element, Long>> firstRoundCheck
+      = parsed.map(new ClassifyFunction())
+      .setParallelism(parallelism);
+
+    DataStream<Tuple2<Element, Long>> firstRoundElements
+      = firstRoundCheck.filter(new ClassifiedFilter());
+
+    DataStream<Tuple2<Element, Long>> firstRoundCentroids
+      = firstRoundCheck.filter(new UnclassifiedFilter());
+
+    DataStream<Tuple3<Long, Element, Double>> secondRoundCheck
+      = firstRoundCentroids.map(new CentroidsGenerator(kTarget));
+
+    DataStream<Tuple3<Long, Element, Double>> secondRoundElements
+      = secondRoundCheck.filter(new RealPoints());
+
+    DataStream<Tuple3<Long, Element, Double>> secondRoundCentroids
+      = secondRoundCheck.filter(new NewCentroids());
+
+    DataStream<Object> distributedCentroids
+      = secondRoundCentroids.flatMap(new CentroidsDistributor(parallelism))
+      .setParallelism(parallelism);
+
+    parsed.closeWith(distributedCentroids);
+  }
 }
